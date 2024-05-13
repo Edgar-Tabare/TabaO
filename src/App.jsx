df2a52e3938cc787277a2dea813b1970f4e45fe6
Tabaré Olivera <edgar.oliveracardozo@gmail.com>
2024-05-13 13:09:50 -0300

    primer cambio

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>taba juegos</h1>
+      <h1>tabare olivera juegos</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
